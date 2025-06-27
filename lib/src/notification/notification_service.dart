cf70735023bce8edbeb682f56718b2decf1b93a9
lucassoares02 <lucas.soares02@hotmail.com>
2025-06-26 21:07:26 -0300

    send information to click notification

--- lib/src/notification/notification_service.dart
+++ lib/src/notification/notification_service.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+import 'package:profair/src/shared/http_service.dart';
+
+final httpService = HttpService();
 
 Future<String> _downloadAndSaveFile(String url, String fileName) async {
   final directory = await getApplicationDocumentsDirectory();
@@ -19,11 +22,19 @@ class NotificationService {
   static Future<void> initialize() async {
     const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('ic_launcher_notification');
 
-    const InitializationSettings initializationSettings = InitializationSettings(
+    final InitializationSettings initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
     );
 
-    await _localNotificationsPlugin.initialize(initializationSettings);
+    await _localNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        final payload = response.payload;
+        if (payload != null) {
+          await _handleNotificationClick(payload);
+        }
+      },
+    );
 
     const AndroidNotificationChannel channel = AndroidNotificationChannel(
       'high_importance_channel',
@@ -34,12 +45,10 @@ class NotificationService {
 
     await _localNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
 
-    // Escuta mensagens enquanto app está em foreground
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       final notification = message.notification;
       final android = message.notification?.android;
-
-      final String? imageUrl = message.notification?.android?.imageUrl ?? message.notification?.android?.imageUrl;
+      final String? imageUrl = android?.imageUrl;
 
       if (notification != null && android != null) {
         final bigPicture = imageUrl != null
@@ -60,6 +69,7 @@ class NotificationService {
               channel.id,
               channel.name,
               channelDescription: channel.description,
+              styleInformation: bigPicture,
               largeIcon: FilePathAndroidBitmap(
                 await _downloadAndSaveFile(imageUrl ?? "https://play-lh.googleusercontent.com/6FINLIOgGm5UN2MuqBIYnqhydb71JlO55aOG1ox_S7WtSGvo-72p5pWkL2OufnIjBbY=w240-h480-rw", 'logo_thumb'),
               ),
@@ -68,6 +78,7 @@ class NotificationService {
               icon: 'ic_launcher_notification',
             ),
           ),
+          payload: "${message.data["notificationId"]} - ${message.data["userId"]}",
         );
       }
     });
@@ -77,6 +88,7 @@ class NotificationService {
     required int id,
     required String title,
     required String body,
+    String? payload,
   }) async {
     const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
       'profair_channel',
@@ -92,6 +104,23 @@ class NotificationService {
       title,
       body,
       generalNotificationDetails,
+      payload: payload,
     );
+  }
+
+  static Future<void> _handleNotificationClick(Object payload) async {
+    try {
+      final response = await httpService.post("notification/opened", {"notification_id": payload});
+      // final response = await http.post(
+      //   Uri.parse('http://192.168.100.86:3001/notification/opened'),
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: '{"notification_id": "$payload"}',
+      // );
+      print('Notificação marcada como aberta:');
+    } catch (e) {
+      print('Erro ao enviar notificação aberta: $e');
+    }
   }
 }
