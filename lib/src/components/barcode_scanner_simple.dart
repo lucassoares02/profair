51d7eaeb64309cb82e66905f036ce9efcc419b60
lucassoares02 <lucas.soares02@hotmail.com>
2025-07-01 09:45:49 -0300

    remove send token in login

--- lib/src/components/barcode_scanner_simple.dart
+++ lib/src/components/barcode_scanner_simple.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -31,7 +30,7 @@ class _BarcodeScannerSimpleState extends State<BarcodeScannerSimple> {
       setState(() {
         _scanned = true;
       });
-      String? token = await FirebaseMessaging.instance.getToken();
+      String? token = "notificationToken";
       print("((((((((((((((((((((((((((((((((((((((((token))))))))))))))))))))))))))))))))))))))))");
       print(token);
 
