e6260d4d88bae9459daad7402170167641a3e21f
lucassoares02 <lucas.soares02@hotmail.com>
2025-07-02 17:33:29 -0300

    test build codemagic ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,102 +1,38 @@
+// import UIKit
+// import Flutter
+
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+    
+//     if #available(iOS 10.0, *) { 
+//      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+//     }
+   
+//     application.registerForRemoteNotifications()
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
+
+
 import UIKit
-import Flutter
+import FirebaseCore
+
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    
-    if #available(iOS 10.0, *) { 
-     UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
-   
-    application.registerForRemoteNotifications()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
   }
 }
-
-
-// import UIKit
-// import Flutter
-// import Firebase
-
-// @UIApplicationMain
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     FirebaseApp.configure()
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
-
-// import UIKit
-// import Flutter
-// import flutter_local_notifications
-
-// @main
-// @objc class AppDelegate: FlutterAppDelegate {
-
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     // This is required to make any communication available in the action isolate.
-//     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-//         GeneratedPluginRegistrant.register(with: registry)
-//     }
-
-//     if #available(iOS 10.0, *) {
-//       UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-//     }
-
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
-
-
-// import UIKit
-// import Flutter
-
-// @UIApplicationMain
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     // 1️⃣ registra plugins do Flutter
-//     GeneratedPluginRegistrant.register(with: self)
-    
-//     // 2️⃣ registra para receber notificações via APNs
-//     application.registerForRemoteNotifications()
-    
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
-
-
-// import UIKit
-// import Flutter
-// import FirebaseMessaging
-
-// @UIApplicationMain
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     GeneratedPluginRegistrant.register(with: self)
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-
-//   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken:Data){
-//       Messaging.messaging().apnsToken = deviceToken
-//       super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-//   }
-// }
