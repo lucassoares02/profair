92d61881043fc638ff10b8e0e4b4f5aef7f9a755
lucassoares02 <lucas.soares02@hotmail.com>
2025-07-02 16:12:17 -0300

    test build codemagic ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -77,7 +77,6 @@
 
 import UIKit
 import Flutter
-import FirebaseCore
 import FirebaseMessaging
 
 @UIApplicationMain
@@ -86,7 +85,7 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
