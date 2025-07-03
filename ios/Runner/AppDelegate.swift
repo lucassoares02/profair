b9e17b9350ce8dcbff2b45d5d3a0a1a3e17438de
lucassoares02 <lucas.soares02@hotmail.com>
2025-07-02 21:30:53 -0300

    add new suggestions to resolve notification module

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -18,23 +18,21 @@ import Flutter
 import UserNotifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    // Define o delegate para capturar notificações em foreground
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
     }
 
-    // Registra o app para notificações push
     application.registerForRemoteNotifications()
 
-    // Inicialização padrão do Flutter
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
